97abe42157c0ce4d66c649f8ec2588d6286e7b34
dzh <d.snitchester@gmail.com>
2026-03-25 22:16:45 +0300

    003_db_context_n_first_migration

--- Api/Controllers/ProductController.cs
+++ Api/Controllers/ProductController.cs
@@ -5,7 +5,7 @@ namespace Api.Controllers
     public class ProductController : StoreController
     {
         [HttpGet]
-        public async Task<IActionResult> Get()
+        public async Task<ActionResult<string>> Get()
         {
             return Ok(await Task.FromResult<string>("_DZHITS"));
         }
--- Api/Program.cs
+++ Api/Program.cs
@@ -1,9 +1,18 @@
+using Api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// настройка взаимодействия приложения с DbContext
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("PostgreSQLConnection"));
+});
 
 var app = builder.Build();
 app.MapControllers();
