3643989a3a37a2a8ac0f36295989cbee8f458cea
dzh <d.snitchester@gmail.com>
2026-05-02 23:05:33 +0300

    022_registering_authentication/authorization_service_n_adding_CORS_policy

--- Api/Program.cs
+++ Api/Program.cs
@@ -14,17 +14,32 @@ builder.Services.AddPostgreSqlIdentityContext();
 builder.Services.AddConfigureIdentityOptions();
 builder.Services.AddJwtTokenGenerator();
 
+// подключение аутентификации через JWT
+builder.Services.AddAuthenticationConfig(builder.Configuration);
+// подключение CORS-политики для разрешения/запрета обработки запросов из различных источников
+builder.Services.AddCors();
+
 builder.Services.AddScoped<IStorage, PostgreSqlEfStorage>();
 
+// сборка приложения и конвейера обработки запросов
 var app = builder.Build();
-app.MapControllers();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline (порядок вызовов в конвейере важен, существуют специальные требования)
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+// определение CORS-политики: разрешение всех заголовков, методов и источников
+app.UseCors(opt =>
+    opt.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin().WithExposedHeaders("*"));
+
+app.UseAuthentication(); // "промежуточное ПО" для обработки аутентификации
+app.UseAuthorization(); // "промежуточное ПО" для определения у аутентифицированного юзера разрешений на доступ к запрашиваемому ресурсу
+
+// middleware (Use...) должны идти до MapControllers (иначе pipeline может работать некорректно)
+app.MapControllers();
 
 await app.Services.InitializeRoleAsync();
 
